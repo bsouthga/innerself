4951a8221196f422fab21c72c3937083bf33b3ff
Ben Southgate <bsouthga@gmail.com>
2017-09-27 19:43:14 -0400

    Updates

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-!function(){"use strict";function e([e,...t],...n){return n.reduce((e,n)=>e.concat(n,t.shift()),[e]).filter(e=>null!=e).join("")}const t=["Home","Examples","Documentation","Download","Github"],n=()=>e`
+!function(){"use strict";function e([e,...t],...n){return n.reduce((e,n)=>e.concat(n,t.shift()),[e]).filter(e=>null!=e).join("")}const t=["Background","Usage"],n=()=>e`
   <div class="navbar">
     <div class="navbar-logo">
       <img src="logo.png"/>
@@ -6,14 +6,17 @@
     </div>
     <div class="navbar-links">
       ${t.map(e=>`\n        <a href="#${e.toLowerCase()}">\n          ${e}\n        </a>\n      `)}
+      <a href="https://github.com/stasm/innerself">
+        Github
+      </a>
     </div>
   </div>
-`,o=()=>e`
+`,i=()=>e`
   <div class="footer">
     © innerself @stas
   </div>
-`,a=(e,t=!1)=>`&lt;${t?"/":""}${e}&gt;`,i=()=>e`
-  <div class="usage">
+`,o=(e,t=!1)=>`&lt;${t?"/":""}${e}&gt;`,a=()=>e`
+  <div id="usage">
     <div class="row">
       <div class="column">
         <h2>
@@ -30,10 +33,10 @@ import ActiveTask from "./ActiveTask";
 
 export default function ActiveList(tasks) {
     return html\`
-        ${a("h2")}My Active Tasks${a("h2",!0)}
-        ${a("ul")}
+        ${o("h2")}My Active Tasks${o("h2",!0)}
+        ${o("ul")}
             \${tasks.map(ActiveTask)}
-        ${a("ul",!0)}
+        ${o("ul",!0)}
     \`;
 }
         </pre>
@@ -66,7 +69,7 @@ export { attach, connect };
   </div>
 `,s=()=>e`
   <div class="background-container">
-    <div class="background">
+    <div id="background">
       <h2>Background</h2>
       <p>
         I wrote innerself because I needed to make sense of the UI for a game I wrote for the js13kGames jam. The whole game had to fit into 13KB. I needed something extremely small which would not make me lose sanity. innerself clocks in at under 50 lines of code. That's around 600 bytes minified, ~350 gzipped.
@@ -75,10 +78,10 @@ export { attach, connect };
   </div>
 `,c=()=>e`
   <div class="content">
-    ${[s(),i()]}
+    ${[s(),a()]}
   </div>
-`,r="INIT",d="INCREMENT",l="DECREMENT",u={count:0},{dispatch:v,connect:h,attach:p}=function(e){function t(){for(const[e,t]of o){const o=t();if(o!==a.get(e)){a.set(e,o),e.innerHTML=o;const t=new CustomEvent("render",{detail:n});e.dispatchEvent(t)}}}let n=e();const o=new Map,a=new Map;return{attach(e,n){o.set(n,e),t()},connect:e=>(...t)=>e(n,...t),dispatch(o,...a){n=e(n,o,a),t()}}}(function(e=u,t={type:r}){switch(t.type){case d:return{count:e.count+t.payload.amount};case l:return{count:e.count-t.payload.amount};default:return e}});window.dispatch=function(e,t){return t?`'event.preventDefault();dispatch(${JSON.stringify(e)})';`:setTimeout(v,0,e)},p(()=>e`
+`,r="INIT",d="INCREMENT",l="DECREMENT",u={count:0},{dispatch:h,connect:v,attach:p}=function(e){function t(){for(const[e,t]of i){const i=t();if(i!==o.get(e)){o.set(e,i),e.innerHTML=i;const t=new CustomEvent("render",{detail:n});e.dispatchEvent(t)}}}let n=e();const i=new Map,o=new Map;return{attach(e,n){i.set(n,e),t()},connect:e=>(...t)=>e(n,...t),dispatch(i,...o){n=e(n,i,o),t()}}}(function(e=u,t={type:r}){switch(t.type){case d:return{count:e.count+t.payload.amount};case l:return{count:e.count-t.payload.amount};default:return e}});window.dispatch=function(e,t){return t?`'event.preventDefault();dispatch(${JSON.stringify(e)})';`:setTimeout(h,0,e)},p(()=>e`
   <div class="container">
-    ${[n(),c(),o()]}
+    ${[n(),c(),i()]}
   </div>
 `,document.getElementById("root"))}();
