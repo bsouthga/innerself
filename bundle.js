9c8f238104de0df600e585f228aa7262c6139819
Ben Southgate <bsouthga@gmail.com>
2017-09-27 18:57:08 -0400

    Updates

--- bundle.js
+++ bundle.js
@@ -1,20 +1,37 @@
-!function(){"use strict";function t([t,...n],...e){return e.reduce((t,e)=>t.concat(e,n.shift()),[t]).filter(t=>null!=t).join("")}function n(t,n){return n?`'event.preventDefault();dispatch(${JSON.stringify(t)})';`:setTimeout(s,0,t)}const e="INIT",c="INCREMENT",o="DECREMENT",u=(t=1)=>({type:c,payload:{amount:t}}),a=(t=1)=>({type:o,payload:{amount:t}}),i={count:0},{dispatch:s,connect:r,attach:d}=function(t){function n(){for(const[t,n]of c){const c=n();if(c!==o.get(t)){o.set(t,c),t.innerHTML=c;const n=new CustomEvent("render",{detail:e});t.dispatchEvent(n)}}}let e=t();const c=new Map,o=new Map;return{attach(t,e){c.set(e,t),n()},connect:t=>(...n)=>t(e,...n),dispatch(c,...o){e=t(e,c,o),n()}}}(function(t=i,n={type:e}){switch(n.type){case c:return{count:t.count+n.payload.amount};case o:return{count:t.count-n.payload.amount};default:return t}});window.dispatch=n;const l=r(e=>t`
-    <div class="counter">
-      <button onclick=${n(u(1),!0)}>
-        increment
-      </button>
-      <button onclick=${n(a(1),!0)}>
-        decrement
-      </button>
+!function(){"use strict";function n([n,...t],...e){return e.reduce((n,e)=>n.concat(e,t.shift()),[n]).filter(n=>null!=n).join("")}const t=["Home","Examples","Documentation","Download","Github"],e=()=>n`
+  <div class="navbar">
+    <div class="navbar-logo">
+      <img src="logo.png"/>
+      <span> innerself </span>
     </div>
-  `);d(r(n=>t`
-  <div class="container">
-    <div class="header">
-      <img src="logo.png" />
-      <h2>
-        you've used an innerself app ${n.count} times...
-      </h2>
+    <div class="navbar-links">
+      ${t.map(n=>`\n        <a href="#${n.toLowerCase()}">\n          ${n}\n        </a>\n      `)}
     </div>
-    ${l()}
   </div>
-`),document.getElementById("root"))}();
+`,s=()=>n`
+  <div class="content">
+    <div class="hero">
+
+    </div>
+    <div class="abstract">
+
+    </div>
+    <div class="background">
+
+    </div>
+    <div class="usage">
+
+    </div>
+    <div class="how-it-works">
+
+    </div>
+  </div>
+`,a=()=>n`
+  <div class="footer">
+    © innerself @stas
+  </div>
+`,i="INIT",c="INCREMENT",o="DECREMENT",d={count:0},{dispatch:r,connect:u,attach:v}=function(n){function t(){for(const[n,t]of s){const s=t();if(s!==a.get(n)){a.set(n,s),n.innerHTML=s;const t=new CustomEvent("render",{detail:e});n.dispatchEvent(t)}}}let e=n();const s=new Map,a=new Map;return{attach(n,e){s.set(e,n),t()},connect:n=>(...t)=>n(e,...t),dispatch(s,...a){e=n(e,s,a),t()}}}(function(n=d,t={type:i}){switch(t.type){case c:return{count:n.count+t.payload.amount};case o:return{count:n.count-t.payload.amount};default:return n}});window.dispatch=function(n,t){return t?`'event.preventDefault();dispatch(${JSON.stringify(n)})';`:setTimeout(r,0,n)},v(()=>n`
+  <div class="container">
+    ${[e(),s(),a()]}
+  </div>
+`,document.getElementById("root"))}();
